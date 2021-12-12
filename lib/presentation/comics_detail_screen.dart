8a7422b75380ad23692fb761f0c25f3879e8bc22
Cristian Devia <c.devia8@gmail.com>
2021-12-12 12:42:44 -0500

    arreglos visuales detail

--- lib/presentation/comics_detail_screen.dart
+++ lib/presentation/comics_detail_screen.dart
@@ -43,25 +43,28 @@ class ComicsDetailScreen extends StatelessWidget {
               ),
               decoration: const BoxDecoration(
                   border: Border(bottom: BorderSide(width: 2)))),
-          detailCharacterCredits.length == 0
-              ? Text("No characters")
-              : ListView.builder(
-                  shrinkWrap: true,
-                  physics: ClampingScrollPhysics(),
-                  itemCount: detailCharacterCredits.length,
-                  itemBuilder: (context, index) {
-                    return Text(
-                      detailCharacterCredits[index]['name'],
-                    );
-                  }),
+          Flexible(
+              child: Container(
+            child: detailCharacterCredits.length == 0
+                ? Text("No characters")
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: detailCharacterCredits.length,
+                    itemBuilder: (context, index) {
+                      return Text(detailCharacterCredits[index]['name'],
+                          overflow: TextOverflow.ellipsis);
+                    }),
+          ))
         ],
       ),
     );
 
     final team = Container(
-        height: Get.height * 0.2,
-        width: Get.width * 0.3,
-        child: Column(children: [
+      height: Get.height * 0.2,
+      width: Get.width * 0.3,
+      child: Column(
+        children: [
           Container(
               width: Get.width * 0.3,
               child: const Text(
@@ -71,49 +74,89 @@ class ComicsDetailScreen extends StatelessWidget {
               ),
               decoration: const BoxDecoration(
                   border: Border(bottom: BorderSide(width: 2)))),
-          detailTeamCredits.length == 0
-              ? Text("No Team")
-              : ListView.builder(
-                  shrinkWrap: true,
-                  physics: ClampingScrollPhysics(),
-                  itemCount: detailTeamCredits.length,
-                  itemBuilder: (context, index) {
-                    return Text(detailTeamCredits[index]['name']);
-                  })
-        ]));
+          Flexible(
+              child: Container(
+            child: detailTeamCredits.length == 0
+                ? Text("No Team")
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: detailTeamCredits.length,
+                    itemBuilder: (context, index) {
+                      return Text(detailTeamCredits[index]['name'],
+                          overflow: TextOverflow.ellipsis);
+                    }),
+          ))
+        ],
+      ),
+    );
 
     final location = Container(
         height: Get.height * 0.2,
         width: Get.width * 0.3,
-        child: detailLocationCredits.length == 0
-            ? Text("No Location")
-            : ListView.builder(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemCount: detailLocationCredits.length,
-                itemBuilder: (context, index) {
-                  return Text(detailLocationCredits[index]['name']);
-                }));
+        child: Column(
+          children: [
+            Container(
+                width: Get.width * 0.3,
+                child: const Text(
+                  "Location",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(width: 2)))),
+            Flexible(
+                child: Container(
+              child: detailLocationCredits.length == 0
+                  ? Text("No Location")
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemCount: detailLocationCredits.length,
+                      itemBuilder: (context, index) {
+                        return Text(detailLocationCredits[index]['name'],
+                            overflow: TextOverflow.ellipsis);
+                      }),
+            ))
+          ],
+        ));
 
     final concepts = Container(
         height: Get.height * 0.2,
         width: Get.width * 0.3,
-        child: detailConcepts.length == 0
-            ? Text("No Concepts")
-            : ListView.builder(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemCount: detailConcepts.length,
-                itemBuilder: (context, index) {
-                  return Text(detailConcepts[index]['name']);
-                }));
+        child: Column(
+          children: [
+            Container(
+                width: Get.width * 0.3,
+                child: const Text(
+                  "Concept",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                decoration: const BoxDecoration(
+                    border: Border(bottom: BorderSide(width: 2)))),
+            Flexible(
+                child: Container(
+              child: detailConcepts.length == 0
+                  ? Text("No Concept")
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemCount: detailConcepts.length,
+                      itemBuilder: (context, index) {
+                        return Text(detailConcepts[index]['name'],
+                            overflow: TextOverflow.ellipsis);
+                      }),
+            ))
+          ],
+        ));
 
     final details = Scaffold(
       appBar: AppBar(
         title: const Text("Comics Details"),
       ),
       body: Container(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
